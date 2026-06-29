1bf7fc128cc5ce0c69782f067ea277a1147a86fe
Pritish <pritishdubeykeck@gmail.com>
2026-06-29 21:06:30 +0530

    Fix Hero ESLint warning

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,29 @@
 import profile from "../assets/Profile.png";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
+
+const roles = [
+  "Frontend Developer",
+  "React Developer",
+  "JavaScript Developer",
+  "Freelancer",
+];
 
 function Hero() {
-    const roles = useMemo(
-  () => [
-    "Frontend Developer",
-    "React Developer",
-    "JavaScript Developer",
-    "Freelancer",
-  ],
-  []
-);
-      
-      const [text, setText] = useState("Frontend Developer");
-      const [index, setIndex] = useState(0);
-      
-      useEffect(() => {
-        const interval = setInterval(() => {
-          setIndex((prev) => (prev + 1) % roles.length);
-        }, 2000);
-      
-        return () => clearInterval(interval);
-      }, []);
-      
-      useEffect(() => {
-        setText(roles[index]);
-      }, [index]);
-                
+  const [text, setText] = useState("Frontend Developer");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % roles.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setText(roles[index]);
+  }, [index]);
+
   return (
     <section className="hero" id="home">
       <div className="hero-content">
@@ -36,33 +33,39 @@ function Hero() {
         </div>
 
         <div className="hero-text">
-          <h1>Hi, I'm <span>Pritish Dubey</span></h1>
+          <h1>
+            Hi, I'm <span>Pritish Dubey</span>
+          </h1>
+
           <h2>{text}</h2>
 
           <p>
-            Frontend Developer with hands-on experience building responsive and user-friendly web applications using React, JavaScript, HTML and CSS.
+            Frontend Developer with hands-on experience building responsive and
+            user-friendly web applications using React, JavaScript, HTML and
+            CSS.
           </p>
 
           <div className="hero-buttons">
-          <a href="#contact"                className="btn-primary">
-            Contact Me
+            <a href="#contact" className="btn-primary">
+              Contact Me
             </a>
 
             <a href="/Pritish_Resume.pdf" download>
               <button>Download Resume</button>
             </a>
           </div>
+
           <div className="hero-tech">
             <span>React</span>
             <span>JavaScript</span>
             <span>HTML</span>
             <span>CSS</span>
             <span>GitHub</span>
-</div>
+          </div>
+
         </div>
-        
+
       </div>
-      
     </section>
   );
 }
